902f502b3a3fd38f526884d7774d804ebff9ea95
Ken Austria <kenaustriadev@gmail.com>
2020-03-17 23:28:04 -0700

    Splitting Components

---
+++ src/scripts/splitting_components.js
@@ -0,0 +1,65 @@
+// filename: splitting_components.js
+// this file must be named index.js
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class MyApp extends Component {
+	state = {
+		cars: [
+			{id: '123', make: 'Toyota', model: 'Celica'},
+			{id: '214', make: 'Nissan', model: 'Altima'},
+			{id: '232', make: 'Honda', model: 'Odyssey'}
+		]
+	}
+
+	render() {
+		let cars;
+		if (this.state.cars) {
+			cars = (
+				<Cars cars={this.state.cars} />
+			)
+		}
+		return (
+			<div>
+				{cars}
+			</div>
+		)
+	}
+}
+
+const Cars = (props) => {
+	return props.cars.map((car, index) => {
+		return (
+			<Car
+				key={car.index}
+				make={car.make}
+				model={car.model}
+			/>
+		)
+	})
+}
+
+const Car = (props) => {
+	return (
+		<div style={carStyles}>
+			<p>I am a {props.make} {props.model}</p>
+		</div>
+	)
+}
+
+const carStyles = {
+	width: '60%',
+	margin: '16px auto',
+	border: '1px solid #eee',
+	boxShadow: '0 2px 3px #ccc',
+	padding: '16px',
+	textAlign: 'center'
+}
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
+
+// App component
+// create car state: type, model
+// define cars variable to render Cars component if state exists
+// define Cars component to map through cars state and render Car component
+// define Car component
